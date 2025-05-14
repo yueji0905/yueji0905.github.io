65946be0d0097ff0fe63d35c101b55ff84c97ab5
yueji0905 <2716447658@qq.com>
2025-05-14 15:25:12 +0800

    Site updated: 2025-05-14 15:25:12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1747205730451";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1747207511810";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",

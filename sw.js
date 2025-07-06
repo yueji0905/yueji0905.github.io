3818a29a6ca43ad41632e1876a0ba2b8bb74d19c
yueji0905 <2716447658@qq.com>
2025-07-06 18:58:09 +0800

    Site updated: 2025-07-06 18:58:09

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1751798725971";const preCache = ["/images/taichi.png","/themes/reimu/source/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1751799488820";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
